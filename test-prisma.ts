9fbb8d7940a8408770ae3a12611f795fac9f97d9
Gimhani <127582749+Gimhani09@users.noreply.github.com>
2025-08-13 13:10:09 +0530

    fixing deployment issues

--- test-prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-// Log all available models
-console.log('Available Prisma models:', Object.keys(prisma))
-
-// Export types for verification
-export * from '@prisma/client'
