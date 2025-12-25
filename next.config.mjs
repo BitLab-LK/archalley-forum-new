e518e9765b814c11b6df20c5ce215f4ef9e76a01
Chavindu Nuwanpriya <chavindun@gmail.com>
2025-12-25 10:09:56 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,8 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.youtube.com https://s.ytimg.com https://www.googletagmanager.com https://vercel.live https://www.payhere.lk https://sandbox.payhere.lk",
-              "script-src-elem 'self' 'unsafe-inline' https://connect.facebook.net https://www.youtube.com https://s.ytimg.com https://www.googletagmanager.com https://vercel.live https://www.payhere.lk https://sandbox.payhere.lk",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.youtube.com https://s.ytimg.com https://www.googletagmanager.com https://vercel.live https://www.payhere.lk https://sandbox.payhere.lk https://www.clarity.ms",
+              "script-src-elem 'self' 'unsafe-inline' https://connect.facebook.net https://www.youtube.com https://s.ytimg.com https://www.googletagmanager.com https://vercel.live https://www.payhere.lk https://sandbox.payhere.lk https://www.clarity.ms",
               "style-src 'self' 'unsafe-inline' https://cdn.lineicons.com",
               "img-src 'self' data: https: https://sandbox.payhere.lk",
               "font-src 'self' data: https://cdn.lineicons.com",
